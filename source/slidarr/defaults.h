81270f9f22597e660cbb8e6a2c7a98c0190b03fc
Mats Jonsson <mats.jonsson@gmail.com>
2019-05-17 17:35:36 +0200

    Set default threshold, add default octave span

--- source/slidarr/defaults.h
+++ source/slidarr/defaults.h
@@ -8,8 +8,9 @@
 #ifndef DEFAULTS_H_
 #define DEFAULTS_H_
 
-#define STRING_THRESHOLD 100 // TODO find a good value
+#define STRING_THRESHOLD 2000 // TODO find a good value
 #define STRING_IDLE_STDDEV 20 // TODO find a good value
+#define DEFAULT_STRING_OCTAVE_SPAN 500
 #define DEFAULT_BASE_FREQUENCY 261.63
 #define PITCHBEND_RESOLUTION 8192
 
