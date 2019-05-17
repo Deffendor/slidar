fbdc67dcd24639a5d0303248545aa59782aa8ed6
Mats Jonsson <mats.jonsson@gmail.com>
2019-05-17 16:37:24 +0200

    Switch to ADC1

--- source/slidarr/adc.c
+++ source/slidarr/adc.c
@@ -14,17 +14,17 @@ void initADC() { // TODO make generic for other ports
     //SYSCTL->RCGCGPIO |= 0x10;
     // TODO: add comments and use better names, also for the ADC stuff a in the endless main while loop
     SYSCTL_RCGCGPIO_R |= 0x10;
-    SYSCTL_RCGCADC_R |= 1;
+    SYSCTL_RCGCADC_R |= 2;
 
-    GPIO_PORTE_AFSEL_R |= 8;
-    GPIO_PORTE_DEN_R &= ~8;
-    GPIO_PORTE_AMSEL_R |= 8;
+    GPIO_PORTE_AFSEL_R |= 4;
+    GPIO_PORTE_DEN_R &= ~4;
+    GPIO_PORTE_AMSEL_R |= 4;
 
-    ADC0_ACTSS_R &= ~8;
-    ADC0_EMUX_R &= ~0xF000;
-    ADC0_SSMUX3_R  = 0;
-    ADC0_SSCTL3_R |= 6;
-    ADC0_ACTSS_R |= 8;
+    ADC1_ACTSS_R &= ~8;
+    ADC1_EMUX_R &= ~0xF000;
+    ADC1_SSMUX3_R  = 1;
+    ADC1_SSCTL3_R |= 6;
+    ADC1_ACTSS_R |= 8;
     /////////////////////// Setup ADC done ///////////////////////////////
 }
 
@@ -32,10 +32,10 @@ void readADC(volatile int *val) { // TODO make generic for other ports
     int current_val;
 
     // read adc
-    ADC0_PSSI_R |= 8;
-    while((ADC0_RIS_R & 8) == 0);
-    *val = ADC0_SSFIFO3_R;
-    ADC0_ISC_R = 8;
+    ADC1_PSSI_R |= 8;
+    while((ADC1_RIS_R & 8) == 0);
+    result2 = ADC1_SSFIFO3_R;
+    ADC1_ISC_R = 8;
 }
 
 
