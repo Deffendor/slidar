d8ab068b343a48e3bb9c576a1d4ef39a85567d7e
Deffendor <Deffendor@DESKTOP-1EGQI2L>
2019-05-30 21:49:35 +0200

    temporary scroll fix, added comments to variables

--- source/slidarr/defaults.h
+++ source/slidarr/defaults.h
@@ -9,7 +9,7 @@
 #define DEFAULTS_H_
 
 #define STRING_THRESHOLD 1000 // TODO find a good value
-#define STRING_IDLE_STDDEV 20 // TODO find a good value
+#define STRING_IDLE_STDDEV 50 // TODO find a good value
 #define DEFAULT_STRING_OCTAVE_SPAN 500
 #define DEFAULT_STRING_BASE 3000
 #define DEFAULT_BASE_FREQUENCY 261.63
@@ -17,7 +17,7 @@
 #define PITCHBEND_MAX_SEMITONES 2
 #define PITCHBEND_INTERVAL 2 // Interval [ms] between pitchbend messages
 
-#define STRING_HISTORY_SIZE 5
+#define STRING_HISTORY_SIZE 40
 #define STRING_SAMPLING_DELAY 5
 
 #define BAUD_RATE 9600 // options are: 9600, 38400, 115200
